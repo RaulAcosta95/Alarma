bcb805c7f8fe7b24bfddca4e72373968756b463f
Raúl Acosta <raulacostaservicio@hotmail.com>
2021-02-27 21:48:58 -0600

    Campos validados, hasta donde yo se

--- js/Script1.js
+++ js/Script1.js
@@ -2,6 +2,9 @@
 var hora,minuto,segundo;
 var recibioTituloAlarma, reciboMensajeAlarma, reciboHoraAlarma, reciboMinutoAlarma;
 var alarmas=[];
+//OJO esta variable validación es para cuando se envían los datos
+//Se modifica en la función validar
+var validación=true;
 
 //OBTEN LA HORA Y MUESTRALA
 function mostrarHora(){
@@ -38,24 +41,54 @@ window.onload=function(){
 }
 
 //RECIBE HORA PARA ALARMAS
+//Funciónes para validad campos
+function validar(titulo,mensaje,numerohora,numerominuto){
+    //Valida campos nulos
+    if (titulo==""||mensaje==""||numerohora==""||numerominuto=="") {
+        console.log(`Nungun campo puede ser nulo`);
+        validación=false;
+    }else{ //Valida la Hora
+        if (numerohora<0 || numerohora>23 || numerohora == '-0') {
+            console.log(`${numerohora} no es una hora valida`);
+            validación=false;
+        }else{ //Valida el minuto
+            if (numerominuto<0 || numerominuto>59 || numerominuto == '-0') {
+                console.log(`${numerominuto} no es un minuto valido`);
+                validación=false;
+            }else{ validación=true } }
+         } 
+}
 //Se ejecuta al apretar boton de "Enviar Alarma"
 function agregaAlarma(){
+    console.log(`Obten los valores de los campos`);
     //Obten los valores de los campos
     recibioTituloAlarma=document.getElementById('Titulo_Alarma').value;
     reciboMensajeAlarma=document.getElementById('Texto_Alarma').value;
     reciboHoraAlarma=document.getElementById('Hora_Alarma').value;
     reciboMinutoAlarma=document.getElementById('Minuto_Alarma').value;
+    //Valida los campos
+    validar(recibioTituloAlarma,reciboMensajeAlarma,reciboHoraAlarma,reciboMinutoAlarma);
     //Guarda los valores en posiciones del arreglo de alarmas
-    alarmas.push(
-        {
-        TituloAlarma:`${recibioTituloAlarma}`,
-        TextoAlarma:`${reciboMensajeAlarma}`,
-        HoraAlarma:`${reciboHoraAlarma}`,
-        MinutoAlarma:`${reciboMinutoAlarma}`
-        }
-    );
+    //Si la validación fue bien, la variable validación será true
+    if (validación) {
+        alarmas.push(
+            {
+            TituloAlarma:`${recibioTituloAlarma}`,
+            TextoAlarma:`${reciboMensajeAlarma}`,
+            HoraAlarma:`${reciboHoraAlarma}`,
+            MinutoAlarma:`${reciboMinutoAlarma}`
+            }
+        );
+        console.log(`Se agregó la alarma ${alarmas.length}`);
+        swal('Bien Hecho!', `Se agrego la alarma: \n${recibioTituloAlarma}`,'success');
+
+    }else{
+          swal('Un problema con tus campos...','No dejes campos vacíos, usa el formato de 24 horas, y cada hora tiene 60 minutos','error');
+    }
+
     return false;
 }
 
 //Como las variables hora, minuto, segundo son globales, se actualizan cada segundo
 //Fuck yeah
+
